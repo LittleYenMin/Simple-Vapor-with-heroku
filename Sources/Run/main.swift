d04768904ac3e71a32b19094e1058bcdb6372a1b
LittleYenMin <fkym08@gmail.com>
2017-08-09 17:57:53 +0800

    cod

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -16,11 +16,9 @@ import Vapor
 ///
 /// .run() runs the Droplet's commands, 
 /// if no command is given, it will default to "serve"
-let config = try Config()
-try config.setup()
 
-let drop = try Droplet(config)
-try drop.setup()
+let drop = try Droplet()
+
 drop.get("/name",":name") { request in
         if let name = request.parameters["name"]?.string {
             return "Hello \(name)!"
